00654edcbcfd19640fcc5b67bf95362627b31196
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-08-23 19:14:49 -0300

    feat: add ky to help http request and config

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,5 +1,14 @@
 'use server'
 
+import { signInWithPassword } from '@/http/sign-in-with-password'
+
 export async function signInWithEmailAndPassword(data: FormData) {
-  console.log(Object.fromEntries(data))
+  const { email, password } = Object.fromEntries(data)
+
+  const result = await signInWithPassword({
+    email: email.toString(),
+    password: password.toString(),
+  })
+
+  console.log(result)
 }
