f7cc26ae99c8b6c879d3caa6562301aac7d99c04
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-09-04 13:24:20 -0300

    feat: create organization action

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,82 +1,11 @@
-import Link from 'next/link'
-
-import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { OrganizationForm } from './organization-form'
 
 export default function CreateOrganization() {
   return (
     <div className="space-y-4">
       <h1 className="text-2xl font-bold">Create organization</h1>
 
-      <form className="space-y-4">
-        {/* {success === false && message && (
-          <Alert variant="destructive">
-            <AlertTriangle className="size-4" />
-            <AlertTitle>Sign in failed!</AlertTitle>
-            <AlertDescription>
-              <p>{message}</p>
-            </AlertDescription>
-          </Alert>
-        )} */}
-
-        <div className="space-y-1">
-          <Label htmlFor="name">Organization name</Label>
-          <Input name="name" id="name" />
-
-          {/* {errors?.name && (
-            <p className="text-xs font-medium text-red-500 dark:text-red-400">
-              {errors.name[0]}
-            </p>
-          )} */}
-        </div>
-
-        <div className="space-y-1">
-          <Label htmlFor="domain">E-mail domain</Label>
-          <Input
-            name="domain"
-            type="text"
-            id="domain"
-            inputMode="url"
-            placeholder="example.com"
-          />
-
-          {/* {errors?.email && (
-            <p className="text-xs font-medium text-red-500 dark:text-red-400">
-              {errors.email[0]}
-            </p>
-          )} */}
-        </div>
-
-        <div className="space-y-1">
-          <div className="items-top flex space-x-2">
-            <Checkbox
-              name="shouldAttachUsersByDomain"
-              id="shouldAttachUsersByDomain"
-            />
-            <div className="grid gap-1.5 leading-none">
-              <label htmlFor="shouldAttachUsersByDomain">
-                <span className="text-sm font-medium leading-none">
-                  Auto-join new members
-                </span>
-                <p className="text-sm text-muted-foreground">
-                  This will automatically invite all members with same e-mail
-                  domain to this organization
-                </p>
-              </label>
-            </div>
-          </div>
-        </div>
-
-        <Button type="submit" className="w-full">
-          Save organization
-        </Button>
-
-        <Button variant="link" className="w-full" size="sm" asChild>
-          <Link href="/auth/sign-in">Already registered? Sign in</Link>
-        </Button>
-      </form>
+      <OrganizationForm />
     </div>
   )
 }
