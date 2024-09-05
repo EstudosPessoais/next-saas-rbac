aaf61de1dc3d3f2323e492e11de977c32f7dc19a
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-09-04 22:53:36 -0300

    fix: layout on pages

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,11 +1,14 @@
+import { Header } from '@/components/header'
+
 import { OrganizationForm } from './organization-form'
 
 export default function CreateOrganization() {
   return (
     <div className="space-y-4">
-      <h1 className="text-2xl font-bold">Create organization</h1>
-
-      <OrganizationForm />
+      <Header />
+      <main className="mx-auto w-full max-w-[1200] space-y-4">
+        <OrganizationForm />
+      </main>
     </div>
   )
 }
--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation'
 
 import { isAuthenticated } from '@/auth/auth'
-import { Header } from '@/components/header'
 
 export default function AuthLayout({
   children,
@@ -12,10 +11,5 @@ export default function AuthLayout({
     redirect('/auth/sign-in')
   }
 
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200]">{children}</main>
-    </div>
-  )
+  return <>{children}</>
 }
